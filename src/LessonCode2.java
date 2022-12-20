978ef46c6a2dcd03d6072d2e4a043b3c4ea5647a
7pin <7pin@mail.ru>
2022-12-21 00:19:15 +0300

    test

---
+++ src/LessonCode2.java
@@ -0,0 +1,15 @@
+public class LessonCode2 {
+
+    public static void main(String[] args) {
+
+        int a = 14;
+        int b = 5;
+
+        int result = a / b;
+        double result2 = a / b;
+
+        System.out.println(result);
+        System.out.println(result2);
+
+    }
+}
