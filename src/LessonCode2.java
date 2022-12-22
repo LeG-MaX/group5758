fa3994a7fd92684de13059fc730a239dd84efd44
7pin <7pin@mail.ru>
2022-12-23 01:14:01 +0300

    test

--- src/LessonCode2.java
+++ src/LessonCode2.java
@@ -5,8 +5,11 @@ public class LessonCode2 {
         int a = 14;
         int b = 5;
 
+        int a1 = a;
+        double b1 = b;
+
         int result = a / b;
-        double result2 = a / b;
+        double result2 = a1 / b1;
 
         System.out.println(result);
         System.out.println(result2);
