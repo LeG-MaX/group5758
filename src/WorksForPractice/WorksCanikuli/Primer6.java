896db3c9c9e1db27fc529395227e56bb30983b56
7pin <7pin@mail.ru>
2022-12-29 01:29:09 +0300

    ispravitOshibki

---
+++ src/WorksForPractice/WorksCanikuli/Primer6.java
@@ -0,0 +1,10 @@
+package WorksForPractice.WorksCanikuli;
+
+public class Primer6 {
+
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+
+        }
+}
