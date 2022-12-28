721e1010109b5e9f6695729aaa8dc75f3afabbab
7pin <7pin@mail.ru>
2022-12-29 00:53:12 +0300

    TablicaUmnojeniya

---
+++ src/WorksForPractice/WorksCanikuli/TablicaUmnojeniya.java
@@ -0,0 +1,23 @@
+package WorksForPractice.WorksCanikuli;
+
+import java.util.Scanner;
+
+public class TablicaUmnojeniya {
+
+        public static void main(String[] args) {
+
+            System.out.println("Enter number from 1 to 10");
+            Scanner scanner = new Scanner(System.in);
+            int inputInteger = scanner.nextInt();
+            System.out.println(inputInteger + " x 1 = " + inputInteger *1);
+            System.out.println(inputInteger + " x 2 = " + inputInteger *2);
+            System.out.println(inputInteger + " x 3 = " + inputInteger *3);
+            System.out.println(inputInteger + " x 4 = " + inputInteger *4);
+            System.out.println(inputInteger + " x 5 = " + inputInteger *5);
+            System.out.println(inputInteger + " x 6 = " + inputInteger *6);
+            System.out.println(inputInteger + " x 7 = " + inputInteger *7);
+            System.out.println(inputInteger + " x 8 = " + inputInteger *8);
+            System.out.println(inputInteger + " x 9 = " + inputInteger *9);
+
+        }
+}
