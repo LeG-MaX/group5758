5f26be2bbe46875b4765428ef8844c64d855355c
7pin <7pin@mail.ru>
2022-12-28 22:28:19 +0300

    names with input

--- src/Lessons/Lesson2/DogDemo2.java
+++ src/Lessons/Lesson2/DogDemo2.java
@@ -6,11 +6,11 @@ public class DogDemo2 {
 
         Dog dog = new Dog();
         Input userInput = new Input();
-        dog.name = userInput;
+        dog.name = userInput.inputString();
 
         Dog dog2 = new Dog();
         Input userInput1 = new Input();
-        dog2.name = userInput1;
+        dog2.name = userInput1.inputString();
 
         System.out.println("My first dog name " + dog.name);
         System.out.println("My second dog name " + dog2.name);
