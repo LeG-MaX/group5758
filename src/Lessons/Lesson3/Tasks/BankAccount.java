98b3f983236406801e078c8347c38f3300a34559
7pin <7pin@mail.ru>
2023-01-22 22:33:50 +0300

    Task17

---
+++ src/Lessons/Lesson3/Tasks/BankAccount.java
@@ -0,0 +1,29 @@
+package Lessons.Lesson3.Tasks;
+
+public class BankAccount {
+
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerFirstName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+    BankAccount (String ownerFirstName,
+                String ownerLastName,
+                int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+}
