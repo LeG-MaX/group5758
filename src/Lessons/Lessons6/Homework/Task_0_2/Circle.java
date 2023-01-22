ae4ebc8fe3b0291b198d748b289f63ebb76a1d92
7pin <7pin@mail.ru>
2023-01-22 22:56:54 +0300

    Task_0_2

---
+++ src/Lessons/Lessons6/Homework/Task_0_2/Circle.java
@@ -0,0 +1,15 @@
+package Lessons.Lessons6.Homework.Task_0_2;
+
+import java.util.Scanner;
+
+public class Circle {
+
+    double radius;
+
+    public Circle() {
+        System.out.println("Insert radius r= ");
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+        System.out.println("P of circle = "+Math.PI*radius*radius);
+    }
+}
---
+++ src/Lessons/Lessons6/Homework/Task_0_2/CircleDemo.java
@@ -0,0 +1,8 @@
+package Lessons.Lessons6.Homework.Task_0_2;
+
+public class CircleDemo {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+    }
+}
