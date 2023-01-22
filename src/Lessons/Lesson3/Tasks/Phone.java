e3305b32e0ee026546792024d53d1a5eb579632a
7pin <7pin@mail.ru>
2023-01-22 22:03:43 +0300

    Task15

--- src/Lessons/Lesson3/Tasks/Phone.java
+++ src/Lessons/Lesson3/Tasks/Phone.java
@@ -4,23 +4,21 @@ public class Phone {
 
     String model;
 
-        Phone(String newModel) {
-            this.model = newModel;
-        }
-
-        String getModel() {
-            return this.model;
-        }
-
+    Phone(String newModel) {
+        this.model = newModel;
     }
 
-    class PhoneDemo {
-
-        public static void main(String[] args) {
-            Phone myPhone = new Phone("Huawei");
-            String phoneModel = myPhone.getModel();
-            System.out.println("Phone model = " + phoneModel);
-        }
-
+    String getModel() {
+        return this.model;
     }
+}
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}
 
