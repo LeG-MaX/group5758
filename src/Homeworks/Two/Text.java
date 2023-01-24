bfb01de60aa22615c6212713f7479724a0a5adc7
7pin <7pin@mail.ru>
2023-01-24 19:06:24 +0300

    Homework2

---
+++ src/Homeworks/Two/Text.java
@@ -0,0 +1,17 @@
+package Homeworks.Two;
+
+public class Text {
+
+    public static void main(String[] args) {
+        String text = "I study Basic Java!";
+        System.out.println(text);
+        System.out.println(text.length());
+        System.out.println(text.charAt(18));
+        System.out.println(text.contains("Java"));
+        System.out.println(text.replace("a","o"));
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.substring(10));
+
+    }
+}
