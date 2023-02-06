a531549d43c78cf5170d6c4e7e7f4fe55724210e
7pin <7pin@mail.ru>
2023-02-06 03:12:44 +0300

    Homework Four

--- src/Homeworks/Four/Task3/ArrayN_1.java
+++ src/Homeworks/Four/Task3/ArrayN_1.java
@@ -9,12 +9,34 @@ public class ArrayN_1 {
     public static void main(String[] args) {
         Array array = new Array();
         int[] NMinusOdin = array.arrayWithNMinusOne();
-        System.out.println(Arrays.toString(NMinusOdin)); //почему при задании последовательного массива
-        // от 1 и выводе массива на экран массив включает 0?
-        System.out.println(NMinusOdin.length);
-        Random random =new Random();
+        System.out.println(Arrays.toString(NMinusOdin));
+  //      System.out.println(NMinusOdin.length);
 
-  //      int[] randomNMinus1 =
+        int n = NMinusOdin.length -1;
+        int N = NMinusOdin[n];
+        int summaAllElements = N*(N+1)/2;
 
+        int summaWhithoutOneElement =0;
+        for (int i = 0; i < NMinusOdin.length; i++) {
+            summaWhithoutOneElement = summaWhithoutOneElement +NMinusOdin[i];
+        }
+        int missingElemet = summaAllElements - summaWhithoutOneElement;
+        System.out.println("Пропущенное число - " + missingElemet);
+        // вариает вызова метода из аррау
+//
+//        int missingElement = array.missingNumber(NMinusOdin[i]);
+
+
+
+
+
+//        int missingElemet = summaAllElements - summaWhithoutOneElement;
+//        for (int i = NMinusOdin.length; i >= 0 ; i--) {
+//            if (((NMinusOdin[i] - 1) - NMinusOdin[i-1]) == 0) {
+//                int missingElemet = NMinusOdin[i] - 1;
+//        System.out.println("Пропущенное число - " + missingElement);
+//
+//            }
+//        }
     }
 }
--- src/Homeworks/Four/Task3/Metods/Array.java
+++ src/Homeworks/Four/Task3/Metods/Array.java
@@ -16,14 +16,30 @@ public class Array {
 
     public int[] arrayWithNMinusOne() {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int size = N;
+        int n = scanner.nextInt();
+        int size = n-1;
         int[] arrayN = new int[size];
-        for (int i = 0; i < arrayN.length ; i++) {
-            if (i != (size-1)) {
+        for (int i = 0; i < (arrayN.length) ; i++) {
+            if (i < (size-1)) {
                 arrayN[i] = 1+i;
+            } else {
+                arrayN[i] = 2+i;
             }
         }
         return arrayN;
     }
+
+public void missingNumber(int[] arrayN){
+    int n = arrayN.length -1;
+    int N = arrayN[n];
+    int summaAllElements = N*(N+1)/2;
+
+    int summaWhithoutOneElement =0;
+    for (int i = 0; i < arrayN.length; i++) {
+        summaWhithoutOneElement = summaWhithoutOneElement +arrayN[i];
+    }
+    int missingElemet = summaAllElements - summaWhithoutOneElement;
+}
+
+
 }
